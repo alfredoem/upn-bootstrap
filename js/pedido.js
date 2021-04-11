7534cec4d82211c0a4d41a2d82ffefe7254da899
Alfredo Espiritu Monago <alfredo.espiritu.m@gmail.com>
2021-04-10 22:47:46 -0500

    wip final

--- js/pedido.js
+++ js/pedido.js
@@ -11,7 +11,7 @@ function validaciones(pedido) {
         return false;
     }
 
-    if (pedido['dni'].trim() === "" && pedido['dni'].trim().match(/[0-9]{8}/)) {
+    if (pedido['dni'].trim() === "" || !pedido['dni'].trim().match(/[0-9]{8}/)) {
         document.getElementById("divMessages").innerHTML = '<div class="alert alert-danger" role="alert">' +
             'Complete su número de DNI' +
             '</div>';
@@ -25,8 +25,31 @@ function validaciones(pedido) {
         return false;
     }
 
+    if (pedido['producto'].trim() === "(*) Selecciona tu producto") {
+        document.getElementById("divMessages").innerHTML = '<div class="alert alert-danger" role="alert">' +
+            'Debe seleccionar un producto' +
+            '</div>';
+        return false;
+    }
 
     return true;
+}
+
+function mostrarPedidos() {
+    tabla_pedidos = document.getElementById("tablePedidos");
+    pedidos_html = '';
+    lista_pedidos.forEach(function (pedido, numero_pedido){
+        pedidos_html +=  '<tr>' +
+            '                    <th scope="row">' + numero_pedido + '</th>' +
+            '                    <td>' + pedido['nombre'] + '</td>' +
+            '                    <td>' + pedido['direccion'] + '</td>' +
+            '                    <td>' + pedido['telefono'] + '</td>' +
+            '                    <td>' + pedido['producto'] + '</td>' +
+            '                    <td>' + pedido['medio_pago'] + '</td>' +
+            '                </tr>';
+    });
+
+    tabla_pedidos.innerHTML = pedidos_html;
 }
 
 function registrarPedido() {
@@ -36,23 +59,19 @@ function registrarPedido() {
     pedido['nombre'] = document.getElementById("txtName").value;
     pedido['dni'] = document.getElementById("txtDocumentNumber").value;
     pedido['direccion'] = document.getElementById("txtAddress").value;
-    pedido['referencia'] = document.getElementById("txtReference").value;
     pedido['telefono'] = document.getElementById("txtPhoneNumber").value;
     select = document.getElementById("selProduct");
     pedido['producto'] = document.getElementById("selProduct").options[select.selectedIndex].text;
 
+    if (document.getElementById('rbtEfectivo').checked) {
+        pedido['medio_pago'] = document.getElementById('rbtEfectivo').value;
+    } else {
+        pedido['medio_pago'] = document.getElementById('rbtTarjeta').value;
+    }
+
     if (validaciones(pedido) === true) {
         lista_pedidos[numero_pedido] = pedido;
-
-        tabla_pedidos = document.getElementById("tablePedidos");
-        tabla_pedidos.innerHTML = tabla_pedidos.innerHTML + '<tr>\n' +
-            '                    <th scope="row">' + numero_pedido + '</th>\n' +
-            '                    <td>Mark</td>\n' +
-            '                    <td>Otto</td>\n' +
-            '                    <td>@mdo</td>\n' +
-            '                </tr>';
-
-
+        mostrarPedidos();
         numero_pedido++;
     }
 
